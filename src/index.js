2dc0adfe74d8e525213853a093c91ac3353fea40
sho40 <kanazawa.sho40@gmail.com>
2020-04-25 15:55:23 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import {Todo} from './components/main'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+var tasksList = ["task 1", "task 2", " task 3"];
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+ReactDOM.render(<Todo tasks={tasksList} />, document.getElementById('todo'));
 serviceWorker.unregister();
